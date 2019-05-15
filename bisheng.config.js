48e7aefae22ddc9cd0449052be7626d5e17d242e
ltaoo <litaowork@aliyun.com>
2019-05-15 22:33:31 +0800

    feat: 确定 md 文件夹结构

--- bisheng.config.js
+++ bisheng.config.js
@@ -23,15 +23,47 @@ const path = require('path');
 // }
 
 module.exports = {
-    source: './posts',
+    source: './components',
     output: './_site',
     theme: 'bisheng-theme-antd',
     themeConfig: {
-        home: '/',
-        sitename: 'One',
-        // plugins: [
-        //     'bisheng-plugin-react?lang=__react',
-        // ],
+        categoryOrder: {
+            'Ant Design': 0,
+            原则: 1,
+            Principles: 1,
+            视觉: 2,
+            Visual: 2,
+            模式: 3,
+            Patterns: 3,
+            其他: 6,
+            Other: 6,
+            Components: 100,
+        },
+        // 控制左侧菜单
+        typeOrder: {
+            General: 0,
+            Layout: 1,
+            Navigation: 2,
+            'Data Entry': 3,
+            'Data Display': 4,
+            Feedback: 5,
+            Other: 6,
+            通用: 0,
+            布局: 1,
+            导航: 2,
+            数据录入: 3,
+            数据展示: 4,
+            反馈: 5,
+            其他: 6,
+        },
+        docVersions: {
+            '0.9.x': 'http://09x.ant.design',
+            '0.10.x': 'http://010x.ant.design',
+            '0.11.x': 'http://011x.ant.design',
+            '0.12.x': 'http://012x.ant.design',
+            '1.x': 'http://1x.ant.design',
+            '2.x': 'http://2x.ant.design',
+        },
     },
     lessConfig: {
         javascriptEnabled: true,
---
+++ components/form/index.md
@@ -0,0 +1,15 @@
+---
+title: Form
+publishDate: 2016-05-05
+tags: 
+  - test
+---
+
+Form 表单示例
+
+---
+
+
+```__react
+ReactDOM.render(<button>click</button>, mountNode);
+```
